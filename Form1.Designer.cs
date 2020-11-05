b66c91b5e0bbc8017eb48500f238b90d19259ab3
zeeTheProgrammer <zeeshanahmedd0010@gmail.com>
2020-11-06 00:59:37 +0500

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace _1st_mid_practice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnShow = new System.Windows.Forms.Button();
+            this.txtBox = new System.Windows.Forms.RichTextBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnColor = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.rbTrue = new System.Windows.Forms.RadioButton();
+            this.rbFalse = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblColor = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnFont = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.rbfFalse = new System.Windows.Forms.RadioButton();
+            this.rbfTrue = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(660, 213);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(126, 23);
+            this.btnShow.TabIndex = 1;
+            this.btnShow.Text = "Open";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // txtBox
+            // 
+            this.txtBox.Location = new System.Drawing.Point(131, 87);
+            this.txtBox.Name = "txtBox";
+            this.txtBox.Size = new System.Drawing.Size(469, 218);
+            this.txtBox.TabIndex = 2;
+            this.txtBox.Text = "";
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(660, 281);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(126, 23);
+            this.btnUpdate.TabIndex = 3;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnColor
+            // 
+            this.btnColor.Location = new System.Drawing.Point(525, 58);
+            this.btnColor.Name = "btnColor";
+            this.btnColor.Size = new System.Drawing.Size(75, 23);
+            this.btnColor.TabIndex = 4;
+            this.btnColor.Text = "Color";
+            this.btnColor.UseVisualStyleBackColor = true;
+            this.btnColor.Click += new System.EventHandler(this.btnColor_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.rbFalse);
+            this.panel1.Controls.Add(this.rbTrue);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(606, 40);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(207, 41);
+            this.panel1.TabIndex = 5;
+            // 
+            // rbTrue
+            // 
+            this.rbTrue.AutoSize = true;
+            this.rbTrue.Checked = true;
+            this.rbTrue.Location = new System.Drawing.Point(3, 19);
+            this.rbTrue.Name = "rbTrue";
+            this.rbTrue.Size = new System.Drawing.Size(47, 17);
+            this.rbTrue.TabIndex = 0;
+            this.rbTrue.TabStop = true;
+            this.rbTrue.Text = "True";
+            this.rbTrue.UseVisualStyleBackColor = true;
+            // 
+            // rbFalse
+            // 
+            this.rbFalse.AutoSize = true;
+            this.rbFalse.Location = new System.Drawing.Point(71, 19);
+            this.rbFalse.Name = "rbFalse";
+            this.rbFalse.Size = new System.Drawing.Size(50, 17);
+            this.rbFalse.TabIndex = 1;
+            this.rbFalse.Text = "False";
+            this.rbFalse.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Show Customized Color Window";
+            // 
+            // lblColor
+            // 
+            this.lblColor.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblColor.Location = new System.Drawing.Point(675, 142);
+            this.lblColor.Name = "lblColor";
+            this.lblColor.Size = new System.Drawing.Size(100, 48);
+            this.lblColor.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(630, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Selected Color";
+            // 
+            // btnFont
+            // 
+            this.btnFont.Location = new System.Drawing.Point(131, 58);
+            this.btnFont.Name = "btnFont";
+            this.btnFont.Size = new System.Drawing.Size(75, 23);
+            this.btnFont.TabIndex = 9;
+            this.btnFont.Text = "Font";
+            this.btnFont.UseVisualStyleBackColor = true;
+            this.btnFont.Click += new System.EventHandler(this.btnFont_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.rbfFalse);
+            this.panel2.Controls.Add(this.rbfTrue);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Location = new System.Drawing.Point(212, 40);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(180, 41);
+            this.panel2.TabIndex = 7;
+            // 
+            // rbfFalse
+            // 
+            this.rbfFalse.AutoSize = true;
+            this.rbfFalse.Location = new System.Drawing.Point(71, 19);
+            this.rbfFalse.Name = "rbfFalse";
+            this.rbfFalse.Size = new System.Drawing.Size(50, 17);
+            this.rbfFalse.TabIndex = 1;
+            this.rbfFalse.Text = "False";
+            this.rbfFalse.UseVisualStyleBackColor = true;
+            // 
+            // rbfTrue
+            // 
+            this.rbfTrue.AutoSize = true;
+            this.rbfTrue.Checked = true;
+            this.rbfTrue.Location = new System.Drawing.Point(3, 19);
+            this.rbfTrue.Name = "rbfTrue";
+            this.rbfTrue.Size = new System.Drawing.Size(47, 17);
+            this.rbfTrue.TabIndex = 0;
+            this.rbfTrue.TabStop = true;
+            this.rbfTrue.Text = "True";
+            this.rbfTrue.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 3);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Show Color Opntion";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 338);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "ZeeWING-Projects";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(660, 247);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(126, 23);
+            this.btnSave.TabIndex = 11;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(834, 351);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.btnFont);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblColor);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnColor);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.txtBox);
+            this.Controls.Add(this.btnShow);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.RichTextBox txtBox;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnColor;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton rbFalse;
+        private System.Windows.Forms.RadioButton rbTrue;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblColor;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnFont;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.RadioButton rbfFalse;
+        private System.Windows.Forms.RadioButton rbfTrue;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1st_mid_practice
+{
+    public partial class Form1 : Form
+    {
+        string filePath;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            string filter = "Zeeshan Files |*.txt;*.java;*.cs|your Files |*.java;*.cs";
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = filter;
+            openFileDialog.FilterIndex = 2;
+            openFileDialog.Multiselect = false;
+            openFileDialog.ShowDialog();
+            filePath = openFileDialog.FileName;
+            string[] value = File.ReadAllLines(openFileDialog.FileName);
+            string x = "";
+            foreach (String v in value)
+            {
+                x = x + (v);
+                x = x + "\n";
+            }
+            txtBox.Text  = x;
+           
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText(filePath, txtBox.Text);
+            MessageBox.Show("Updated");
+        }
+
+        private void btnColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            if (rbFalse.Checked) colorDialog.FullOpen = false;
+            else colorDialog.FullOpen = true;
+            colorDialog.ShowDialog();
+            lblColor.BackColor = txtBox.ForeColor;
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                lblColor.BackColor = colorDialog.Color;
+                txtBox.ForeColor = colorDialog.Color;
+            }
+        
+        }
+
+        private void btnFont_Click(object sender, EventArgs e)
+        {
+            FontDialog fontDialog = new FontDialog();
+            if (rbfFalse.Checked) fontDialog.ShowColor = false;
+            else fontDialog.ShowColor = true;
+
+            fontDialog.ShowDialog();
+            if (fontDialog.ShowDialog() == DialogResult.OK)
+            {
+                txtBox.Font = fontDialog.Font;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                
+                MessageBox.Show(txtBox.Text);
+                File.WriteAllText(saveFileDialog.FileName+".txt", txtBox.Text);
+                MessageBox.Show("Saved");
+            }
+        }
+    }
+    public enum Cities
+    {
+        Sukkur,Larkana,Kandkot,Naudero
+    }
+    public enum ContantNumber: int 
+    {
+        
+    }
+
+}
